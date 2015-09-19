7a97966e2df77f08ca879d8e450c352ce15bd3f8
Jevin Sidhu <jevin.sidhu@gmail.com>
2015-09-19 15:39:39 -0400

    Interface Design + Buttons Completed

--- hack-the-north/DetailViewController.swift
+++ hack-the-north/DetailViewController.swift
@@ -1,21 +1,28 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  hack-the-north
 //
 //  Created by Jevin Sidhu on 2015-09-19.
 //  Copyright (c) 2015 Jevin Sidhu. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class DetailViewController: UIViewController {
+    
+    let image = UIView(frame: CGRectMake(0, 0, 100, 100))
+    
+    @IBAction func backToPeople(segue:UIStoryboardSegue) {
+        
+    }
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -24,10 +31,6 @@ class ViewController: UIViewController {
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
-
-
+    
+    
 }
-
-
-
-
